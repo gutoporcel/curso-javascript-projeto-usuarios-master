d26929f6f268dd3a3ea6333c0a2d9b3f5728f603
porcel1910 <porcel_augusto@hotmail.com>
2021-09-01 12:27:14 -0300

    variáveis e seletores
    Do form do  index

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-alert("hello World");
+var nome = document.querySelector('#exampleInputName');
+var genero =  document.querySelectorAll('#form-user-create [name=gender]:checked');//pegar o conjunto de radio buton
+
+var dataNas = document.querySelector("#exampleInputBirth");
+var pais = document.querySelector("#exampleInputCountry");
+var email = document.querySelector("#exampleInputEmail");
+var senha = document.querySelector("#exampleInputPassword");
+var arquivoFoto = document.querySelector("#exampleInputFile");
+var admin = document.querySelector("#exampleInputAdmin");
+
+nome.value ='oi';
+nome.style.color ='blue';
+console.log(genero);
