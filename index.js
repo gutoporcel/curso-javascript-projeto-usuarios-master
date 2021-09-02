cad0a064feaddce90308ef1cc4c17d23c2f7cf47
porcel1910 <porcel_augusto@hotmail.com>
2021-09-02 12:31:11 -0300

    forEach  para pega valores

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 var nome = document.querySelector('#exampleInputName');
 var genero =  document.querySelectorAll('#form-user-create [name=gender]:checked');//pegar o conjunto de radio buton
-
 var dataNas = document.querySelector("#exampleInputBirth");
 var pais = document.querySelector("#exampleInputCountry");
 var email = document.querySelector("#exampleInputEmail");
 var senha = document.querySelector("#exampleInputPassword");
 var arquivoFoto = document.querySelector("#exampleInputFile");
 var admin = document.querySelector("#exampleInputAdmin");
+/// fields = campos
+var fields = document.querySelectorAll("#form-user-create [name]");
+fields.forEach(function(field,index){
+    if(field.name == "gender"){
 
-nome.value ='oi';
-nome.style.color ='blue';
-console.log(genero);
+        if(field.checked) {
+            console.log ("SIM",field)
+
+        }
+
+
+       // console.log ("SIM",field)
+
+    }else{
+        console.log("Não")
+
+    }
+
+    console.log(field.id);
+
+});
