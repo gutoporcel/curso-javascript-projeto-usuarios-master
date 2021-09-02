3f1c56561a8592a05f01febba998674a0d530c9f
porcel1910 <porcel_augusto@hotmail.com>
2021-09-02 19:48:18 -0300

    pegando os dados do form

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
-var nome = document.querySelector('#exampleInputName');
+/*var nome = document.querySelector('#exampleInputName');
 var genero =  document.querySelectorAll('#form-user-create [name=gender]:checked');//pegar o conjunto de radio buton
 var dataNas = document.querySelector("#exampleInputBirth");
 var pais = document.querySelector("#exampleInputCountry");
 var email = document.querySelector("#exampleInputEmail");
 var senha = document.querySelector("#exampleInputPassword");
 var arquivoFoto = document.querySelector("#exampleInputFile");
-var admin = document.querySelector("#exampleInputAdmin");
+var admin = document.querySelector("#exampleInputAdmin");*/
 /// fields = campos
 var fields = document.querySelectorAll("#form-user-create [name]");
+var user = {};
+document.getElementById("form-user-create").addEventListener("submit",function(event){
+    event.preventDefault();
+
 fields.forEach(function(field,index){
     if(field.name == "gender"){
 
         if(field.checked) {
-            console.log ("SIM",field)
+            user[field.name] = field.value;
+
 
         }
 
-
-       // console.log ("SIM",field)
-
     }else{
-        console.log("Não")
-
+        user[field.name] = field.value;
     }
 
-    console.log(field.id);
+   // console.log(field.id);
+});
 
+console.log(user);
+/*
+document.querySelectorAll("button").forEach(function(){
+    
+    this.addEventListener("click",function(){
+
+        console.log("clicou")
+    });
+
+});*/
+
+
+   console.log("ok");
 });
