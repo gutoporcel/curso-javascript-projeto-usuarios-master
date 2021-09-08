b8a1d73376b9e48fe118dbb4e194918ffd818933
porcel1910 <porcel_augusto@hotmail.com>
2021-09-08 12:02:31 -0300

    inserindo  a foto  pelo fileReader

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -15,7 +15,21 @@ class UserController{
 
         this.formEl.addEventListener("submit" ,event => { //arrow function
             event.preventDefault();
-           this.addLine(this.getValues());
+
+            let values =this.getValues();
+
+            values.photo = "";
+
+            this.getPhoto((content)=>{
+
+                values.photo = content;
+                this.addLine(values);
+
+            });
+
+
+
+           
         
         });
 
@@ -23,6 +37,36 @@ class UserController{
     }
 
 //onsubimt
+
+getPhoto(callback){
+
+let fileReader = new FileReader();
+
+let elements = [...this.formEl.elements].filter(item=>{
+
+if (item.name === 'photo'){
+
+    return item;
+}
+
+});
+
+
+let file = elements[0].files[0];
+fileReader.onload = ()=>{
+
+ callback(fileReader.result);
+
+
+};
+
+fileReader.readAsDataURL(file);
+
+
+
+}
+
+
 
     getValues(){
         let user ={};
@@ -55,7 +99,7 @@ class UserController{
         this.tableEl.innerHTML =`
     
             <tr>
-                <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+                <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
                 <td>${dataUser.name}</td>
                 <td>${dataUser.email}</td>
                 <td>${dataUser.admin}</td>
